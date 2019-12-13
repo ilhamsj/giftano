5d12f61d8454f9313bb46cab5183f6a22b0d882e
ilhamsj <ilhamsaputrajati@gmail.com>
2019-12-13 15:10:48 +0700

    factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -10,12 +10,13 @@ $factory->define(Product::class, function (Faker $faker) {
         'name' => $faker->randomElement(['Doritos', 'Oriental Super Ring', 'Chanberry', 'Bluberry', 'Lays', 'Muruku', 'Indomilk', 'SgMilk']),
         'category_id' => \App\Category::all()->random(),
         'image' => $faker->randomElement([
-                'banana-2019085304.jpg',
-                'bing-cherry-2019124401.jpg',
-                'boysenberries-2019083407.jpg',
-                'grape-2019114258.jpg',
-                'grape-2019114545.jpg',
-                'grape-2019142358.jpg',
+            env('APP_URL').'images/bing-cherry-2019124401.jpg',
+            env('APP_URL').'images/chanberry-2019030251.jpg',
+            env('APP_URL').'images/chanberry-2019030330.jpg',
+            env('APP_URL').'images/doritos-2019080627.jpg',
+            env('APP_URL').'images/oriental-super-ring-2019080641.jpg',
+            env('APP_URL').'images/oriental-super-rong-2019090740.jpg',
+            env('APP_URL').'images/sgmilk-2019080544.jpg',
         ])
     ];
 });
